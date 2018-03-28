d72d31d8de7643a001fa8d7e38348a5ee7b011cd
Anand Bhupathiraju <anandbhupathiraju@icloud.com>
2018-03-28 15:26:57 +0530

    Update bashrc.sh

--- bashrc.sh
+++ bashrc.sh
@@ -10,6 +10,17 @@ echo alias e=exit >> .bashrc
 echo clear >> .bashrc
 echo echo ***Welcome to Root Privilage!*** >> .bashrc
 
+#Hostname 
+hostnamectl status
+hostnamectl set-hostname geeklab    ## static
+hostnamectl set-hostname "Geeks LAB"   ## pretty
+hostnamectl
+cat /etc/hostname
+echo "newḧostname" > /etc/hostname
+cat /etc/hosts
+echo xxx.xxx.xxx.xxx newhostname >> /etc/hosts
+
+
 #!/bin/bash
 yum -y update
 yum -y install wget elinks httpd vim ntp zip unzip git

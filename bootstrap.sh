88744021080afce9fae583567ee727e3d4bfe123
Anand <anandbhupathiraju@icloud.com>
2018-06-25 23:48:31 +0200

    Added
    Git was added

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#The Boot Strap script will create the user and make the user sudo user!
+#The Below lines Configure the git to get start with cloned files.
+mkdir git && cd git
+git config --global user.name "anandzy"
+git config --global user.email "anandbhupathiraju@icloud.com"
+git init
+git clone https://github.com/anandzy/mypublicode
 
+exit 0

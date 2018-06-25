f002a665073ec4b36c16135547f3da82cf024958
Anand <anandbhupathiraju@icloud.com>
2018-06-26 00:10:11 +0200

    +
    added git to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 #The Below lines Configure the git to get start with cloned files.
+sudo yum -y install git
+#The below two lines can be ignore if you really care about memory.
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel
+sudo git config --global user.name "Anand Bhupathiraju"
+sudo git config --global user.email "anandbhupathiraju@icloud.com"
 mkdir git && cd git
-git config --global user.name "anandzy"
-git config --global user.email "anandbhupathiraju@icloud.com"
-git init
-git clone https://github.com/anandzy/mypublicode
+sudo git init
+sudo git clone https://github.com/anandzy/mypublicode
+sudo chmod 777 /home/ec2-user/git/mypublicode/*
+sudo sh /home/ec2-user/git/mypublicode/basicosreqs.sh
 
 exit 0

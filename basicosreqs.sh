c118c60af534e3b01218258230509af51ab605c9
Anand <anandbhupathiraju@icloud.com>
2018-06-25 23:48:12 +0200

    git
    The git lines were joined in to bootstrap script.

--- basicosreqs.sh
+++ basicosreqs.sh
@@ -59,12 +59,6 @@ sudo rm .vimrc; touch .vimrc; echo se nu >> .vimrc
 #The below code will create the user 
 useradd anand
 
-#The Below lines Configure the git to get start with cloned files.
-mkdir git && cd git
-git config --global user.name "anandzy"
-git config --global user.email "anandbhupathiraju@icloud.com"
-git init
-git clone https://github.com/anandzy/mypublicode
-
-exit 0
+#The below lines will make the services restart which are installed by the script!
+service httpd restart
 
